9242581a4478c25a21be0c386fe0ad606c8b7992
Shinsuke Sugaya <shinsuke@apache.org>
2024-11-02 13:47:51 +0900

    fess 14.17.0

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,9 +2,9 @@
 
 base_dir=$(cd $(dirname $0);cd ..;pwd)
 fess_plugins="
-fess-script-groovy:14.14.0
-fess-ds-git:14.14.0
-fess-theme-codesearch:14.14.0
+fess-script-groovy:14.17.0
+fess-ds-git:14.17.0
+fess-theme-codesearch:14.17.0
 "
 
 if [ $(uname -s) = "Linux" ] ; then
@@ -14,6 +14,7 @@ fi
 
 echo "Creating directories..."
 mkdir -p ${base_dir}/data/https-portal/ssl_certs
+mkdir -p ${base_dir}/data/fess/home/fess
 mkdir -p ${base_dir}/data/fess/opt/fess
 mkdir -p ${base_dir}/data/fess/var/lib/fess
 mkdir -p ${base_dir}/data/fess/var/log/fess
@@ -35,18 +36,22 @@ for fess_plugin in ${fess_plugins} ; do
   curl -s -o ${plugin_file} \
     https://repo1.maven.org/maven2/org/codelibs/fess/${plugin_name}/${plugin_version}/${plugin_name}-${plugin_version}.jar
   if [[ ${plugin_name} = "fess-theme-codesearch" ]] ; then
-    rm -rf ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/view/codesearch/*
-    unzip ${plugin_file} "view/*" -d ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/view/codesearch
-    rm -rf ${base_dir}/data/fess/usr/share/fess/app/css/codesearch/*
-    unzip ${plugin_file} "css/*" -d ${base_dir}/data/fess/usr/share/fess/app/css/codesearch
-    rm -rf ${base_dir}/data/fess/usr/share/fess/app/images/codesearch/*
-    unzip ${plugin_file} "images/*" -d ${base_dir}/data/fess/usr/share/fess/app/images/codesearch
+    rm -rf ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/view/codesearch
+    unzip ${plugin_file} "view/*" -d ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/view
+    mv ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/view/view ${base_dir}/data/fess/usr/share/fess/app/WEB-INF/view/codesearch
+    rm -rf ${base_dir}/data/fess/usr/share/fess/app/css/codesearch
+    unzip ${plugin_file} "css/*" -d ${base_dir}/data/fess/usr/share/fess/app/css
+    mv ${base_dir}/data/fess/usr/share/fess/app/css/css ${base_dir}/data/fess/usr/share/fess/app/css/codesearch
+    rm -rf ${base_dir}/data/fess/usr/share/fess/app/images/codesearch
+    unzip ${plugin_file} "images/*" -d ${base_dir}/data/fess/usr/share/fess/app/images
+    mv ${base_dir}/data/fess/usr/share/fess/app/images/images ${base_dir}/data/fess/usr/share/fess/app/images/codesearch
   fi
 done
 
 if [ $(uname -s) = "Linux" ] ; then
   echo "Changing an owner for directories..."
   sudo chown -R root ${base_dir}/data/https-portal/ssl_certs
+  sudo chown -R 1001 ${base_dir}/data/fess/home/fess
   sudo chown -R 1001 ${base_dir}/data/fess/opt/fess
   sudo chown -R 1001 ${base_dir}/data/fess/var/lib/fess
   sudo chown -R 1001 ${base_dir}/data/fess/var/log/fess
